57b31c109b01f0e18b68bcc656a9d1ad41154e64
sabina <sabina.moreno.morlon@alumnos.upm.es>
2023-09-30 19:25:18 +0200

    Configuración del la Autenticación para la api

---
+++ src/main/java/api/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package api;
+
+import com.google.common.annotations.Beta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain SecurityWebFilterChain(ServerHttpSecurity httpSecurity){
+        httpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+}
